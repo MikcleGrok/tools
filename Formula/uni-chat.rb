d730181d06d7cdf27feae946dda90f2c1c67a889
Sergey <sergey@Sergeys-MacBook-Pro.local>
2026-08-29 02:00:06 +0300

    Publish signed uni-chat Homebrew formulae

--- Formula/uni-chat.rb
+++ Formula/uni-chat.rb
@@ -1,15 +1,15 @@
 class UniChat < Formula
   desc "Personal chat notifier, TUI and poster"
   homepage "https://github.com/MikcleGrok/uni-chat"
-  version "1.4.15"
+  version "1.4.53"
   release_asset = "uni-chat-#{version}-darwin-arm64.tar.gz"
   artifact = ENV["HOMEBREW_UNI_CHAT_ARTIFACT"]
   if artifact
     url "file://#{artifact}"
     sha256 ENV.fetch("HOMEBREW_UNI_CHAT_ARTIFACT_SHA256")
   else
-    url "https://github.com/MikcleGrok/uni-chat/releases/download/v#{version}/#{release_asset}"
-    sha256 "a8d8e5807afcbd9037c8c0a3c011ef96996caa2517b321996e5f804710f854e8"
+    url "https://github.com/MikcleGrok/uni-chat/releases/download/v1.4.53/uni-chat-1.4.53-darwin-arm64.tar.gz"
+    sha256 "9c906e27d354c6fec3cec9829777d99e1daf747c73e1537fc75b26dc17048b67"
   end
   depends_on :macos
   depends_on "terminal-notifier"
